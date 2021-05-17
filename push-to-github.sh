1bfa3661cc3b360af324e0cedb135f94b0559164
mebsp <62543386+mebsp@users.noreply.github.com>
2021-05-17 19:10:30 +0100

    refreshed script

---
+++ push-to-github.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd git-resource
+ls -lah > listing.txt
+echo >> listing.txt;
+date >> listing.txt;
+git config --global user.name "mebsp"
+git config --global user.email "mehboob.patel@smarsh.com"
+git remote add origin https://github.com/mebsp/simple-deploy-test.git
+git init
+git add .
+git commit -m "Commit by Concourse 233"
+git push origin
